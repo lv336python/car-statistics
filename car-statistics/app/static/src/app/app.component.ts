6c00e5576004fbe47cf55b8b0df67b9dca0f6ac5
AnyaShymanska <ani4ka30@gmail.com>
2018-09-21 00:27:08 +0300

    Add angular_notification

--- car-statistics/app/static/src/app/app.component.ts
+++ car-statistics/app/static/src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NotificationService } from './notification.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  title = 'app';
+
+  constructor(private notification: NotificationService){ }
+
+  ngOnInit() {
+    this.notification.messages.subscribe(msg => {
+      console.log(msg);
+    })
+  }
+
+  sendMessage() {
+    this.notification.sendMsg("Test Message");
+  }
 
 }
+
+
